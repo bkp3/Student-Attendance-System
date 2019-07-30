78a96b597884408a4fb930cac4a25ac043394eb8
bkp5 <48831618+bkp5@users.noreply.github.com>
2019-07-30 14:21:46 +0530

    New project

---
+++ import_to_db.bat
@@ -0,0 +1,7 @@
+@ECHO ON 
+REM Runs both my project scripts
+
+C:\Users\BKP\AppData\Local\Programs\Python\Python37-32\Python.exe C:\Users\BKP\PycharmProjects\attendence_management_system\input.py
+ECHO Attendance imporetd to Database Successfully........
+
+PAUSE
---
+++ monthly_extract.bat
@@ -0,0 +1,7 @@
+@ECHO ON 
+REM Runs both my project scripts
+
+C:\Users\BKP\AppData\Local\Programs\Python\Python37-32\Python.exe C:\Users\BKP\PycharmProjects\attendence_management_system\monthly_extract.py
+ECHO Attendance exported to student_details_for_a_month.csv Successfully........
+
+PAUSE
